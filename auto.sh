b3487597d1994dc6b57615721542e8b1caf8177a
linfeitang <linfeipro95@163.com>
2018-03-21 17:55:54 +0800

    自动化

---
+++ auto.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cat i3config > ~/.i3/config
+cat i3statusconfig > ~/.config/i3status/config
+cat .bashrc > ~/.bashrc
+cat .tmux.conf > ~/.tmux.conf
+cat .nethackrc > ~/.nethackrc
+cat .Xresources > ~/.Xresources
+cat .xinitrc > ~/.xinitrc
+
+cp /etc/X11/xinit/xserverrc ~/.xserverrc
