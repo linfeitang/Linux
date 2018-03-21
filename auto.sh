ec7f3a9f617fbbd470e48f83d6122c19803ec8c9
linfeitang <linfeipro95@163.com>
2018-03-21 18:17:27 +0800

    自动化

--- auto.sh
+++ auto.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-mkdir ~/.i3
+cat .bashrc > ~/.bashrc
+cat .nethackrc > ~/.nethackrc
+cat .tmux.conf > ~/.tmux.conf
+cat .xinitrc > ~/.xinitrc
+cat .Xresources > ~/.Xresources
 cat i3config > ~/.i3/config
 cat i3statusconfig > ~/.config/i3status/config
-cat .bashrc > ~/.bashrc
-cat .tmux.conf > ~/.tmux.conf
-cat .nethackrc > ~/.nethackrc
-cat .Xresources > ~/.Xresources
-cat .xinitrc > ~/.xinitrc
-
 cp /etc/X11/xinit/xserverrc ~/.xserverrc
+mkdir ~/.config/i3status
+mkdir ~/.i3
